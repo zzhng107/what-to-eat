84635de6b42ad653b31ae48ca151f5bd23b510d2
Zhiwei Zhang <zzw408673530@gmail.com>
2017-12-08 23:40:24 -0600

    distance calculation api

--- backend/routes/restaurants.js
+++ backend/routes/restaurants.js
@@ -50,7 +50,8 @@ router.get('/', function(req,res){
 				let user_tag;
 				let out;
 				console.log("{email:"+req.query.email+"}");
-				users.findone()
+				// users.findone(JSON.parse("{email:"+req.query.email+"}"))
+				users.findOne()
 				.exec((err, user_t)=>{
 					if(err){
 						res.status(500).send({
