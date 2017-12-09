b7f6e867ff96bcb74f6f6854320d0769ed2482f8
Alan Jin <xilunjin7@gmail.com>
2017-12-09 03:19:45 -0600

    Add like and dislike API

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -13,28 +13,22 @@ module.exports = (passport)=> {
     var express = require('express'), 
         router = express.Router(),
         users  = require('../models/userSchema');
-        restaurants  = require('../models/dishSchema')
+        dishes  = require('../models/dishSchema')
         mongoose = require('mongoose')
 
     router.post('/register',
         passport.authenticate('local-signup'),
         (req, res) => {
-            let where = {$where:"this.dishes.length == 1"}
-            restaurants.find(where, function(err,res_restaurants){
-                console.log(req.isAuthenticated());
-                res.status(200).json({ user: req.user, message: "Welcome!",data:res_restaurants});
-            });
-    });
+            res.status(200).json({ user: req.user.email});
+        }
+    );
 
     router.post('/login',
         passport.authenticate('local-login'),
         (req, res) => {
-            let where = {$where:"this.dishes.length == 1"}
-            restaurants.find(where, function(err,res_restaurants){
-                console.log(req.isAuthenticated());
-                res.status(200).json({ user: req.user, message: "Welcome!",data:res_restaurants});
-            });
-    });
+            res.status(200).json({ user: req.user.email}); 
+        }
+    );
 
     router.get('/logout', (req, res) => {
         req.logOut();
@@ -57,10 +51,10 @@ module.exports = (passport)=> {
                 inc[dish_tag] = dish_tags[dish_tag];
             }
             if(!isEmpty(inc)){
-                update_info[""]
+                update_info["$inc"] = inc;
             }
 
-            users.update({email:req.user.email},{$push:{dish_like:dish_id}},{new:true}, (err,res_user)=>{
+            users.update({email:req.user.email},update,(err,res_user)=>{
                 if(err){
                     res.status(500).send(err);
                     return;
@@ -68,18 +62,40 @@ module.exports = (passport)=> {
                 res.status(200).send("Updated "+ req.user.email);
 
             });
-
-        //users.findByIdAndUpdate({email:req.user.email},{$set:{name:"Naomi"}}, {new: true}, function(err,user){
-        //    if(err){
-        //        console.log("Something wrong when updating data!");
-        //    }
-        
-        //    console.log(doc);
-        //})
-       
-        
         });
-    })
+    });
+
+    router.put('/dislike', (req, res) => {
+        
+        let dish_id = req.body.dish_id;
+        dishes.findOne({_id:dish_id}, (err, res_dish)=>{
+            if(err){
+                res.status(500).send(err);
+                return;
+            }
+            let update_info = {};
+            update_info["$push"] = {dish_dislike:dish_id};
+            let dish_tags = res_dish.tag;
+            let inc = {};
+            for(dish_tag in dish_tags){
+                inc[dish_tag] = dish_tags[dish_tag];
+            }
+            if(!isEmpty(inc)){
+                update_info["$inc"] = -inc;
+            }
+
+            users.update({email:req.user.email},update,(err,res_user)=>{
+                if(err){
+                    res.status(500).send(err);
+                    return;
+                }
+                res.status(200).send("Updated "+ req.user.email);
+
+            });
+        });
+    });
+
+        
     return router;
 }
 
